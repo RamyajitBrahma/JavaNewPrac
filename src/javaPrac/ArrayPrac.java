acf10bdcf5edc37340c9b5c15c1b4dd8cc8e7c35
RamyajitBrahma <Ramyajit.Bramha@gmil.com>
2020-06-29 16:33:09 +0100

    29th June changes

--- src/javaPrac/ArrayPrac.java
+++ src/javaPrac/ArrayPrac.java
@@ -7,11 +7,15 @@ public class ArrayPrac {
           int a[] = new int[6];
           String b[] = new String[4];
           int aa[] = {1,3,5,6};
+          int[] aaArr = {1,5,6,7}; // also can declare array like this
           int am[][] = new int[2][3];
           int bm[][] = {{1,2,3},{3,4,5}};
           
           System.out.println("col size:"+ bm[0].length);
           System.out.println("Array muylti size:"+ bm.length);
+          System.out.println(aaArr[1]);
+          System.out.println(aaArr.length);
+          
 	}
 	
 
